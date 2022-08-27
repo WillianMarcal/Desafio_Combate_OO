21423d7431ce715f3b7cb9ea4bae25bdde9bac1f
Willian Marcal <willianpetermarcal@gmail.com>
2022-08-27 01:40:35 +0100

    Final_Final_Commit

--- src/application/Program.java
+++ src/application/Program.java
@@ -47,31 +47,23 @@ public class Program {
         armor = sc.nextInt();
         Champion c2 = new Champion(name, life, attack, armor);
       
-       c1.takeDamage(c2);
-       c2.takeDamage(c1); 
-       
        System.out.println();
        System.out.print("How many round would you like? " );
        int round = sc.nextInt();
        
        System.out.println();
-       for(int i = 1; i <= round; i++){
+       
+       for(int i = 1; i <= round && c1.getLife() > 0 && c2.getLife() > 0; i++){
             System.out.println("Final Result round "+ i + ":");
+            c1.takeDamage(c2);
+            c2.takeDamage(c1);
             System.out.println(c1.status());
             System.out.println(c2.status());
-           
-             if(c1.getLife() == 0 || c2.getLife() == 0){
-                i = 4;
-                 System.out.println();
-             }else{
-                System.out.println("");
-                c1.takeDamage(c2);
-                c2.takeDamage(c1);
-             }
-        }    
+            System.out.println();
+            
+        }
        
         System.out.println("END OF FIGHT");
-       
        
        sc.close();
         
